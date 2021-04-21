da3fd1a359096f7d9ad5cc9e2171f3229d4ba83a
Ivan <sychyov-86@mail.ru>
2021-04-21 14:05:03 +0500

    game --  Guess Number

---
+++ main.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+
+def binary_search(number):
+    '''Бинарный поиск смысл заключаеться в том, что берём определённый интервал, в нашем случае от 0 до 100
+     и сравниваем больше или меньше полученное число от загадоного и в зависимости от результата берём тот диапозон где лежит число.....'''
+    count = 1
+    end_number = 100/2
+    home_number = 0
+    while end_number != number:
+        count += 1
+        if end_number > number:
+            end_number = home_number + round((end_number - home_number)/ 2)
+        else:
+            home_number = end_number
+            end_number = home_number + round(((end_number*2)-home_number)/ 2)
+    return count
+
+
+def game_core_v2(number):
+    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток'''
+    count = 1
+    predict = np.random.randint(1, 101)
+    while number != predict:
+        count += 1
+        if number > predict:
+            predict += 1
+        elif number < predict:
+            predict -= 1
+    return count
+    # выход из цикла, если угадали
+
+
+def game_core_v1(number):
+    '''Просто угадываем на random, никак не используя информацию о больше или меньше.
+       Функция принимает загаданное число и возвращает число попыток'''
+    count = 0
+    while True:
+        count += 1
+        predict = np.random.randint(1, 101)  # предполагаемое число
+        if number == predict:
+            return count  # выход из цикла, если угадали
+
+
+def score_game(game_core):
+    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1, 101, size=1000)
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return (score)
+
+
+print(score_game(binary_search))
